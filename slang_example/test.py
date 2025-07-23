11a2f3d87abd410fa1433f3fb31f32d9b97c0025
Jiahui Liang <bigjack@JiahuideMacBook-Air.local>
2025-07-23 18:30:37 +0800

    more

--- slang_example/test.py
+++ slang_example/test.py
@@ -12,13 +12,27 @@ for mod in tree.root.members:
         if isinstance(mem, pyslang.PortDeclarationSyntax):
             print(f"{mem.declarators}")
             print(f"port_decl_header: {mem.header}")
-            first_token = mem.header.getFirstToken()
-            print(f"raw_text: {first_token.rawText}")
-            print(f"trivia: {first_token.trivia}")
-            print(f"value: {first_token.value}")
-            print(f"valueText: {first_token.valueText}")
-            last_token = mem.header.getLastToken()
+            #first_token = mem.header.getFirstToken()
+            #print(f"raw_text: {first_token.rawText}")
+            #print(f"trivia: {first_token.trivia}")
+            #print(f"value: {first_token.value}")
+            #print(f"valueText: {first_token.valueText}")
+            #print(f"value: {first_token.value}")
+            #last_token = mem.header.getLastToken()
             print("=====================================")
+        elif isinstance(mem, pyslang.DataDeclarationSyntax):
+            print(f"{mem.declarators}")
+        elif isinstance(mem, pyslang.ProceduralBlockSyntax):
+        #ProceduralBlockSyntax -> MemberSyntax -> SyntaxNode
+        # keywork: Token,   statement: StatementSynatax    
+            print(f"{mem.kind}")
+            print(f"parent_kind:{mem.parent.kind}")
+            print(f"source_range_1: ({mem.sourceRange.start}, {mem.sourceRange.end})")
+            print(f"source_range_2: ({mem.sourceRange.start.offset}, {mem.sourceRange.end.offset})")
+            print(f"token: {str(mem.keyword)}")
+            print("=====================================")
+            
+        
         print(f"{mem.kind}")
     #print(mod.members[1].header.dataType)
     count = count + 1
